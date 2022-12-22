1b0681efd4778bbb347545798df8c359b71c9f53
Tin <dotinvta2@gmail.com>
2022-12-22 22:26:22 +0700

    add responsive

--- components/Project_Item.tsx
+++ components/Project_Item.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { faWalkieTalkie } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Project_Item(props) {
+export default function Project_Item(props: any) {
   return (
     <div className="project_section_item">
       <FontAwesomeIcon
         className="project_section_item_icon"
         icon={faWalkieTalkie}
       ></FontAwesomeIcon>
+      s
       <div className="project_item_content">
         <div className="project_section_item_title">{props.title}</div>
         <div className="project_section_item_desc">{props.content}</div>
